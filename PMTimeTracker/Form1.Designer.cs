bccf7debc7a8d183ee26096f538e94387e813b64
pauliver <pauliver@gmail.com>
2023-05-12 12:34:39 -0700

    draws a pie chart

--- PMTimeTracker/Form1.Designer.cs
+++ PMTimeTracker/Form1.Designer.cs
@@ -28,16 +28,19 @@
       /// </summary>
       private void InitializeComponent()
       {
+         this.components = new System.ComponentModel.Container();
          System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TimeTracking));
          this.ExitBtn = new System.Windows.Forms.Button();
          this.HideBtn = new System.Windows.Forms.Button();
+         this.Timer = new System.Windows.Forms.Timer(this.components);
+         this.DrawingPanel = new System.Windows.Forms.Panel();
          this.SuspendLayout();
          // 
          // ExitBtn
          // 
-         this.ExitBtn.Location = new System.Drawing.Point(480, 400);
+         this.ExitBtn.Location = new System.Drawing.Point(564, 12);
          this.ExitBtn.Name = "ExitBtn";
-         this.ExitBtn.Size = new System.Drawing.Size(114, 26);
+         this.ExitBtn.Size = new System.Drawing.Size(67, 26);
          this.ExitBtn.TabIndex = 0;
          this.ExitBtn.Text = "Exit";
          this.ExitBtn.UseVisualStyleBackColor = true;
@@ -45,13 +48,29 @@
          // 
          // HideBtn
          // 
-         this.HideBtn.Location = new System.Drawing.Point(480, 368);
+         this.HideBtn.Location = new System.Drawing.Point(564, 432);
          this.HideBtn.Name = "HideBtn";
-         this.HideBtn.Size = new System.Drawing.Size(114, 26);
+         this.HideBtn.Size = new System.Drawing.Size(67, 26);
          this.HideBtn.TabIndex = 1;
          this.HideBtn.Text = "Hide";
          this.HideBtn.UseVisualStyleBackColor = true;
          this.HideBtn.Click += new System.EventHandler(this.Hide_Click);
+         // 
+         // Timer
+         // 
+         this.Timer.Interval = 1000;
+         this.Timer.Tick += new System.EventHandler(this.Timer_Tick);
+         // 
+         // DrawingPanel
+         // 
+         this.DrawingPanel.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("DrawingPanel.BackgroundImage")));
+         this.DrawingPanel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+         this.DrawingPanel.ForeColor = System.Drawing.SystemColors.Window;
+         this.DrawingPanel.Location = new System.Drawing.Point(1, 3);
+         this.DrawingPanel.Name = "DrawingPanel";
+         this.DrawingPanel.Size = new System.Drawing.Size(637, 466);
+         this.DrawingPanel.TabIndex = 2;
+         this.DrawingPanel.DoubleClick += new System.EventHandler(this.DrawingPanel_Click);
          // 
          // TimeTracking
          // 
@@ -59,8 +78,9 @@
          this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
          this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
          this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-         this.ClientSize = new System.Drawing.Size(606, 438);
+         this.ClientSize = new System.Drawing.Size(634, 461);
          this.ControlBox = false;
+         this.Controls.Add(this.DrawingPanel);
          this.Controls.Add(this.HideBtn);
          this.Controls.Add(this.ExitBtn);
          this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
@@ -79,6 +99,8 @@
 
       private System.Windows.Forms.Button ExitBtn;
       private System.Windows.Forms.Button HideBtn;
+      private System.Windows.Forms.Timer Timer;
+      private System.Windows.Forms.Panel DrawingPanel;
    }
 }
 
--- PMTimeTracker/Form1.cs
+++ PMTimeTracker/Form1.cs
@@ -12,6 +12,10 @@ namespace PMTimeTracker
 {
    public partial class TimeTracking : Form
    {
+      bool TimerActive = false;
+      int accumulated_seconds = 0;
+      int timeout = 60 * 60; // 1 hour
+      bool LastHourTimedOut = false;
       private System.Windows.Forms.NotifyIcon notifyIcon1;
       public TimeTracking()
       {
@@ -29,7 +33,9 @@ namespace PMTimeTracker
       }
 
       private void Form1_Load(object sender, EventArgs e)
-      {  
+      {
+         myPieGraphic = DrawingPanel.CreateGraphics();
+         DrawingPanel.Hide();
       }
 
       private void Exit_Click(object sender, EventArgs e)
@@ -44,6 +50,72 @@ namespace PMTimeTracker
       private void Show_Click(object sender, EventArgs e)
       {
          this.Show();
+         DrawPieChart(myPiePercent, myPieColors);
+      }
+
+      private void Timer_Tick(object sender, EventArgs e)
+      {
+         if (TimerActive)
+         {
+            accumulated_seconds += 1;
+         }
+         if(accumulated_seconds > timeout)
+         {
+            TimerActive = false;
+            accumulated_seconds = 0;
+            LastHourTimedOut = true;
+         }
+      }
+
+      int[] myPiePercent = { 10, 20, 25, 5, 40 };
+      Color[] myPieColors = { Color.Red, Color.Black, Color.Blue, Color.Green, Color.Maroon };
+      Graphics myPieGraphic;
+
+      public void DrawPieChart(int[] myPiePerecents, Color[] myPieColors)
+      {
+         //myPieGraphic.Clear(Color.White);
+         DrawingPanel.Show();
+         Point myPieLocation = new Point(125, 50);
+
+         //Set Here Size Of The Chartâ€¦
+         Size myPieSize = new Size(400, 400);
+
+         //Check if sections add up to 100.
+         int sum = 0;
+         foreach (int percent_loopVariable in myPiePerecents)
+         {
+            sum += percent_loopVariable;
+         }
+
+         if (sum != 100)
+         {
+            MessageBox.Show("Sum Do Not Add Up To 100.");
+         }
+
+         //Check Here Number Of Values & Colors Are Same Or Not.They Must Be Same.
+         if (myPiePerecents.Length != myPieColors.Length)
+         {
+            MessageBox.Show("There Must Be The Same Number Of Percents And Colors.");
+         }
+
+         int PiePercentTotal = 0;
+         for (int PiePercents = 0; PiePercents < myPiePerecents.Length; PiePercents++)
+         {
+            using (SolidBrush brush = new SolidBrush(myPieColors[PiePercents]))
+            {
+
+               //Here it Will Convert Each Value Into 360, So Total Into 360 & Then It Will Draw A Full Pie Chart.
+               myPieGraphic.FillPie(brush, new Rectangle(myPieLocation, myPieSize), Convert.ToSingle(PiePercentTotal * 360 / 100), Convert.ToSingle(myPiePerecents[PiePercents] * 360 / 100));
+            }
+
+            PiePercentTotal += myPiePerecents[PiePercents];
+         }
+         return;
+      }
+
+      private void DrawingPanel_Click(object sender, EventArgs e)
+      {
+         DrawingPanel.Hide();
       }
    }
 }
